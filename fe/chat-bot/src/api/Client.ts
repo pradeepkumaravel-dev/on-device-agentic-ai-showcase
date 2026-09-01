8f2142e62af30900979ebe3895d2d6f8bca4855e
pradeep.k <pradeep.kumaravel.work@gmail.com>
2026-09-01 12:49:27 +0530

    hard coding base url
    It's safe as the entire solution will run in local

--- fe/chat-bot/src/api/Client.ts
+++ fe/chat-bot/src/api/Client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL : import.meta.env.VITE_API_BASE_URL,
+    baseURL : "http://localhost:8080",
     headers : {'Content-Type':'application/json'},
 });
